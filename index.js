1587b736bb3bd648d909571fa22c6e6c0748587c
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-14 18:04:22 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1= 62;
+let num2=1;
+const multiply= num1*num2;
+// console.log(multiply)
+
+
+// Math.random()
+let random = Math.floor(Math.random()*10)+1
+console.log(random)
+
+let num3=9
+let num4=5
+
+const mod = num3%num4
+console.log(mod)
+
+let max=Math.max(2,4,6,18,20);
+console.log(max)
